2b01e78aa6904f126239c14787e192b1371db6a7
Abhishek Yadav <110240550+abhimom03@users.noreply.github.com>
2023-03-06 13:48:07 +0530

    updated all syntaxes

--- controllers/image.js
+++ controllers/image.js
@@ -1,27 +1,3 @@
-//clarifai api
-
-// const handleApiCall = (req, res) => {
-//     fetch(`https://api.clarifai.com/v2/models/face-detection/outputs`, {
-//       method: 'POST',
-//       headers: {
-//           'Accept': 'application/json',
-//           'Authorization': 'Key ' + 'e5c122ec6f3045a49898ca6d909b602e'
-//       },
-//       body: JSON.stringify({
-//         "inputs": [
-//             {
-//                 "data": {
-//                     "image": {
-//                         "url": `${req.body.input}`
-//                     }
-//                 }
-//             }
-//         ]
-//       })
-//   })
-//   .then(response => res.json(response))
-//   .catch(err => res.status(400).json('unable to work with API'))
-// }
 
 const handleImageGet = (req, res, db) => {
     const { id } = req.body;
@@ -35,6 +11,5 @@ const handleImageGet = (req, res, db) => {
 }
 
 module.exports = {
-    handleImageGet,
-    // handleApiCall
+    handleImageGet
 }   
